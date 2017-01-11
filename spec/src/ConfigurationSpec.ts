6a3001a25e41a5fde55d4a85c68ad214140053bf
BlackSpotBear <blackspotbear@users.noreply.github.com>
2017-01-11 10:51:09 +0900

    initial commit

---
+++ spec/src/ConfigurationSpec.ts
@@ -0,0 +1,34 @@
+import * as cmn from "@akashic/akashic-cli-commons";
+import { Configuration } from "../../lib/Configuration";
+
+describe("Configuration", function () {
+	describe("#removeOperationPlugin()", function () {
+		it("removes declaration", function () {
+			var logger = new cmn.ConsoleLogger({ debugLogMethod: () => {/* do nothing*/} })
+			var content = {
+				width: 120,
+				height: 120,
+				fps: 30,
+				operationPlugins: [
+					{ code: 10, script: "foo" },
+					{ code: 12, script: "bar" },
+					{ code: 8, script: "anotherScript", options: { optvalue: true } },
+				]
+			};
+			var self = new Configuration({ content, logger });
+			self.removeOperationPlugin("bar@0.2");
+			expect(self.getContent().operationPlugins).toEqual([
+				{ code: 10, script: "foo" },
+				{ code: 8, script: "anotherScript", options: { optvalue: true } },
+			]);
+		});
+
+		it("nothing removed for module not declared in operationPlugins", function () {
+			var logger = new cmn.ConsoleLogger({ debugLogMethod: () => {/* do nothing*/} })
+			var content = { width: 120, height: 120, fps: 30 };
+			var self = new Configuration({ content, logger });
+			self.removeOperationPlugin("foo");
+			expect(self.getContent().operationPlugins).toEqual([]);
+		});
+	});
+});
